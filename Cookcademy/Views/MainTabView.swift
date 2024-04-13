15a223c668807b2084105cc4b7c216d4b52bc798
Timneji <139955017+timseoaudit@users.noreply.github.com>
2024-04-13 05:58:35 +0100

    add Favorites and TabView as home screen between GridView and Favorites. Big Bug could be solved is commend out in RecipeListView than works as intended

---
+++ Cookcademy/Views/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Cookcademy
+//
+//  Created by user on 13/04/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
